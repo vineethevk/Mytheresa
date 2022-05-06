337ac01c80ea1a5710a66d470809437086d68e0f
Vineeth <96077560+vineethevk@users.noreply.github.com>
2022-05-06 22:15:40 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { Route, Routes } from "react-router-dom"
-// import { Dropdown } from "./components/Navbar/Dropdown"
-// import { Navbar } from "./components/Navbar/Navbar"
-import { Test } from "./Pages/Test"
+import { Navbar } from "./components/Navbar/Navbar"
 import { Womens } from "./Pages/women/Womens"
 import { WomensList } from "./Pages/women/WomensProduct"
 
@@ -9,10 +7,10 @@ function App() {
 
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <Routes>
         <Route path="/womens" element={<Womens />}></Route>
-        <Route path="/womensproduct" element={<WomensList />}></Route>
+        <Route path="/womenproducts" element={<WomensList />}></Route>
         {/* <Route path="/" element={<Dropdown data={dropdown} />}></Route> */}
       </Routes>
     </div>
