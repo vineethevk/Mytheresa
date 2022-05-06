5ce1eed4c9f127d94605d45298f89145255e31b2
Vineeth <96077560+vineethevk@users.noreply.github.com>
2022-05-06 22:33:23 +0530

    Revert "first commit"

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { Route, Routes } from "react-router-dom"
+import { Dropdown } from "./components/Navbar/Dropdown"
 import { Navbar } from "./components/Navbar/Navbar"
-import { Womens } from "./Pages/women/Womens"
-import { WomensList } from "./Pages/women/WomensProduct"
-import {Womens} from "./Pages/women/Womens"
+import { Test } from "./Pages/Test"
 
 function App() {
 
@@ -10,8 +9,6 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route path="/womens" element={<Womens />}></Route>
-        <Route path="/womenproducts" element={<WomensList />}></Route>
         {/* <Route path="/" element={<Dropdown data={dropdown} />}></Route> */}
       </Routes>
     </div>
