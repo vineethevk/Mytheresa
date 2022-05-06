a4e37454a4706f4cf91bae5c7d38347e56eaf8de
Karishma282 <karishmanikam282@gmail.com>
2022-05-06 19:30:10 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom"
 import { Dropdown } from "./components/Navbar/Dropdown"
 import { Navbar } from "./components/Navbar/Navbar"
 import { Test } from "./Pages/Test"
+import { WomensList } from "./Pages/women/WomensProduct"
+import {WomensProduct} from "./Pages/women/Womens"
 
 function App() {
 
@@ -9,6 +11,8 @@ function App() {
     <div>
       <Navbar />
       <Routes>
+        <Route path="/Womens" element={<WomensList/>}></Route>
+        <Route path="/WomensProduct" element={<WomensProduct/>}></Route>
         {/* <Route path="/" element={<Dropdown data={dropdown} />}></Route> */}
       </Routes>
     </div>
--- src/Pages/women/WomensProduct.jsx
+++ src/Pages/women/WomensProduct.jsx
@@ -1,7 +1,7 @@
 import { useEffect ,useState } from "react";
 import axios from "axios";
 import{Link} from "react-router-dom"
-import "../components/WomenProduct.css"
+import "./WomenProduct.css"
 
 export const WomensList = () => {
 
