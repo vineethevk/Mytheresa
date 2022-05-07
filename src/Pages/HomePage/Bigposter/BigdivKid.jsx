e187f463f643994301f387e719b8672f388d8093
Vineeth <96077560+vineethevk@users.noreply.github.com>
2022-05-07 22:13:40 +0530

    update

---
+++ src/Pages/HomePage/Bigposter/BigdivKid.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import "./BigdivKid.css"
+
+export const BigdivKid=()=>{
+    return(
+        <Link to="/">
+        <div className="myimgkid">
+            <div className="kidDiv">Kids</div>
+            
+        </div>
+        
+    </Link>
+    )
+        
+}
---
+++ src/components/Navbar/TopNav.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import { AiOutlineHeart } from 'react-icons/ai'
+import './TopNav.css'
+export const TopNav = () => {
+    return (
+        <div className='TopNav'>
+            <div className="TopLeft">
+                <ul>
+                    <li><Link to="/women">WOMEN</Link></li>
+                    <li><Link to="/men">MEN</Link></li>
+                    <li><Link to="/">KIDS</Link></li>
+                </ul>
+            </div>
+            <div className='TopRight'>
+                <ul>
+                    <li><Link to="/">Signup for Newsletter</Link></li>
+                    <li><Link to="/">My account</Link></li>
+                    <li><Link to="/">My wishlist {<AiOutlineHeart />}</Link></li>
+                    <li><Link to="/">International | English</Link></li>
+                </ul>
+            </div>
+        </div>
+    )
+}
