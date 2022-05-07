5ee33746ac33dc7eb90efa06249578f1f3dfe978
deepak7843 <deepakshankardubey123@gmail.com>
2022-05-07 11:49:07 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Footer } from "./components/Footer/footer"
 import { Route, Routes } from "react-router-dom"
 import { Dropdown } from "./components/Navbar/Dropdown"
 import { Navbar } from "./components/Navbar/Navbar"
@@ -9,8 +10,10 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        {/* <Route path="/" element={<Dropdown data={dropdown} />}></Route> */}
+        <Route path="/" element={<Navbar />}></Route>
       </Routes>
+
+      <Footer />
     </div>
   )
 }
